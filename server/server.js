eaf5b98699e39de57d89902387fe1e82205554cb
kgontarz124 <katarzynagontarz124@gmail.com>
2018-08-13 14:36:57 +0200

    Setup app for heroku

--- server/server.js
+++ server/server.js
@@ -57,8 +57,9 @@ app.get("/todos/:id", (req, res) => {
   // res.send(req.params);
 });
 
-app.listen(3000, () => {
-  console.log("Start on port 3000");
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Start on port ${port}`);
 });
 
 module.exports = { app };
